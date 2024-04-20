e345cada38ff5c38124645e60b44a9f1fd9c00b8
21120575 <nguyen.thanhtri2112@gmail.com>
2024-04-20 10:10:00 +0700

    [Update] Decorate for ftms & display

--- task3/fe/src/App.js
+++ task3/fe/src/App.js
@@ -1,41 +1,57 @@
 import './App.css';
-import {useState, useEffect} from "react"
-import data from './data.json'
+import { useState, useEffect } from "react";
+import data from './data.json';
 import createStore from './redux/createStore';
+import TreeDisplay from './utils/TreeDisplay';
+import Card from './components/Card';
+
 function App() {
-  const [members, setMembers] = useState([])
-  const [loading, setLoadding] = useState(true);
-
-  // create the family tree list 
-  const initFT = () => {
-    setMembers(data)
-    setLoadding(false)
-  }
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    initFT();
-  }, [loading])
-  
+    if (!loading || !members) {
+      const cont = document.querySelector("#FamilyTree");
+      const card_dim = { w: 220, h: 70, text_x: 75, text_y: 15 };
+      const card_display = cardDisplay();
+      const store = createStore({
+        data: members,
+        node_separation: 250,
+        level_separation: 150,
+      });
+      const view = TreeDisplay({
+        store,
+        cont: cont,
+      });
+
+      const UserCard = Card({
+        store,
+        svg: view.svg,
+        card_dim: card_dim,
+        card_display: card_display,
+      });
+      view.setCard(UserCard);
+      store.setOnUpdate((props) => view.update(props || {}));
+      store.update.tree({ initial: true });
+    }
+  }, [loading, members]);
+
   const cardDisplay = () => {
-    const fullname = d => `${d.data['firstName'] || ''} ${d.data['lastName'] || ''}`
-    fullname.create_form = "{firstName} {lastName}"
-    return [fullname]
-  }
+    return [
+      (d) => `${d.data["firstName"] || ""} ${d.data["lastName"] || ""}`,
+    ];
+  };
 
   useEffect(() => {
-    if (loading || !members) return;
-    
-    const card_dim = {w:220,h:70,text_x:75,text_y:15};
-    const card_display = cardDisplay();
-    const store = createStore({
-      data: members,
-      node_separation: 250, 
-      level_separation: 150,
-    })
-  })
+    setMembers(data);
+    setLoading(false);
+    console.log(data);
+  }, []);
+
   return (
-    <div className="App">
-        
+    <div className="ftms" id="FamilyTree">
+      <div className="tree-cont">
+      </div>
     </div>
   );
 }
